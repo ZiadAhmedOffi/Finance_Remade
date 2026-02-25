4215aafc66997910b423101f1549d1a22cce7c54
ZiadAhmedOffi <ziadahmedoffi@gmail.com>
2026-02-25 09:46:24 +0200

    Made some tests to the backend application.

--- backend/backend_app/settings.py
+++ backend/backend_app/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
-print("LOADING THE WSGI SETTINGS")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
+    'users',
 ]
 
 MIDDLEWARE = [
