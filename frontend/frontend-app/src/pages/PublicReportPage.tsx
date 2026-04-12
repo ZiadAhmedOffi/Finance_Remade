b729fb35f98cddda14b57a3f37fa12d8ea98c5b8
ZiadAhmedOffi <ziadahmedoffi@gmail.com>
2026-04-12 05:43:27 +0200

    Edited PublicReportPag.tsx to handle vercel deployment errors.

--- frontend/frontend-app/src/pages/PublicReportPage.tsx
+++ frontend/frontend-app/src/pages/PublicReportPage.tsx
@@ -240,7 +240,7 @@ const PublicReportPage: React.FC = () => {
                     cx="50%" cy="50%"
                     outerRadius={80}
                     dataKey="value"
-                    label={({ name, percent }) => `${name} (${(percent * 100).toFixed(0)}%)`}
+                    label={({ name, percent }) => `${name} (${((percent || 0) * 100).toFixed(0)}%)`}
                   >
                     {chartDataSectorCount.map((_, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)}
                   </Pie>
@@ -262,7 +262,7 @@ const PublicReportPage: React.FC = () => {
                     cx="50%" cy="50%"
                     outerRadius={80}
                     dataKey="value"
-                    label={({ name, percent }) => `${name} (${(percent * 100).toFixed(0)}%)`}
+                    label={({ name, percent }) => `${name} (${((percent || 0) * 100).toFixed(0)}%)`}
                   >
                     {chartDataSectorCapital.map((_, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)}
                   </Pie>
