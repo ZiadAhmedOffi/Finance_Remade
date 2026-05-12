fb65946e1ac5179a9fb6bd2728b981b5a91d97e0
ZiadAhmedOffi <141055239+ZiadAhmedOffi@users.noreply.github.com>
2026-05-12 16:42:24 +0300

    Modify E2E test command to keep the database

--- backend/build.sh
+++ backend/build.sh
@@ -4,4 +4,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 echo "Running E2E tests..."
-python manage.py test funds.test_e2e_deals
+python manage.py test funds.test_e2e_deals --keepdb
