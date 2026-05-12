755af88a7d59b5dd69bbd3823af09ad7cb92c4f8
ZiadAhmedOffi <141055239+ZiadAhmedOffi@users.noreply.github.com>
2026-05-12 16:44:18 +0300

    Restore migration and E2E test commands
    Remove E2E test commands in build script.

--- backend/build.sh
+++ backend/build.sh
@@ -3,5 +3,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-echo "Running E2E tests..."
-python manage.py test funds.test_e2e_deals --keepdb
